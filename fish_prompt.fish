bbc23d868d78891252a2bed1e759b96b3341c8bb
Barbz <BarbzYHOOL@users.noreply.github.com>
2018-05-13 03:49:43 +0200

    Fix the jobs number not showing (#5)
    * Fix the jobs number not showing
    
    Before, it only showed one job
    
    Now it lists the number of jobs instead

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -112,7 +112,7 @@ function __theme_print_git_status
 end
 function __theme_print_jobs
     [ "$theme_display_jobs" = 'no' ]; and return
-    set -l num_jobs (jobs -l | command wc -l)
+    set -l num_jobs (jobs -c | command wc -l)
 
     if [ $num_jobs -gt 0 -o "$theme_display_jobs_always" = "yes" ]
         print_colored "$theme_prompt_status_jobs_char" $theme_color_status_prefix
