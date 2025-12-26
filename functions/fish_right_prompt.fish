1a03bbafba18e609d0ad424f7ab46827005c58f0
Beau Hastings <beau@saweet.net>
2025-12-26 09:51:55 -0600

    remove: redundant variable in fish_right_prompt
    - Remove unnecessary 'st' variable assignment
    - Use  directly in the echo statement

--- functions/fish_right_prompt.fish
+++ functions/fish_right_prompt.fish
@@ -1,7 +1,5 @@
 function fish_right_prompt
-    set -l st $status
-
     if test $status -ne 0
-        echo -n (set_color $theme_color_error) ↵ $st(set_color $theme_color_normal)
+        echo -n (set_color $theme_color_error) ↵ $status(set_color $theme_color_normal)
     end
 end
