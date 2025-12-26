860f0923c99c9423f4f157ef8e2dd0117a81f07b
Beau Hastings <beau@saweet.net>
2025-12-26 09:41:39 -0600

    update: prompt formatting
    - Remove trailing newlines from prompt output using echo -n
    - Improve status check syntax in right prompt (use test instead of [)
    
    Signed-off-by: Beau Hastings <beau@saweet.net>

--- functions/fish_right_prompt.fish
+++ functions/fish_right_prompt.fish
@@ -1,7 +1,7 @@
 function fish_right_prompt
     set -l st $status
 
-    if [ $status != 0 ]
-        echo (set_color $theme_color_error) ↵ $st(set_color $theme_color_normal)
+    if test $status -ne 0
+        echo -n (set_color $theme_color_error) ↵ $st(set_color $theme_color_normal)
     end
 end
