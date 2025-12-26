2824170cb6fe6abd4346e8e678763fa0e94790fb
Beau Hastings <beau@saweet.net>
2025-12-26 09:52:13 -0600

    fix: global variable mutation in __theme_print_superuser
    - Explicitly use 'set -g' to set global variable
    - Change comparison to use 'test' with numeric comparison (-eq)
    - Makes variable scope explicit and prevents potential side effects

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -205,11 +205,11 @@ function __theme_print_pwd_rw
     print_colored $rw_chars $theme_color_status_rw
 end
 function __theme_print_superuser
-    if [ (command id -u) = "0" ]
-        set theme_prompt_char "$theme_prompt_char_superuser"
+    if test (command id -u) -eq 0
+        set -g theme_prompt_char "$theme_prompt_char_superuser"
         print_colored $theme_prompt_superuser_glyph $theme_color_superuser
     else
-        set theme_prompt_char "$theme_prompt_char_normal"
+        set -g theme_prompt_char "$theme_prompt_char_normal"
     end
 end
 function __theme_print_user
